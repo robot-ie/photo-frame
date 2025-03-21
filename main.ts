c6277e7bcd5eaf542e72e071b6d6a33cdce03c0b
robot-ie <mig.ruiz+1@gmail.com>
2025-03-21 15:21:22 +0000

    groups

--- main.ts
+++ main.ts
@@ -6,59 +6,62 @@ namespace photoFrame {
 
 
     //% block
-    //% group="Basic"
+    //% group="Lights"
     export function turnLedAtPosition(position: number) {
         photoFrame.lights.turnLedAt(position)
     }
 
     //% block
-    //% group="Basic"
+    //% group="Screen"
     export function showPhotoAtPosition(position: number) {
         photoFrame.display.showPhotoAtPosition(position)
     }
 
 
     //% block
-    //% group="Basic"
+    //% group="Buttons"
     export function onLeftButtonActive(handler: () => void) {
         photoFrame.navigation.onLeftButtonChanged(handler)
     }
 
     //% block
-    //% group="Basic"
+    //% group="Buttons"
     export function onRightButtonActive(handler: () => void) {
         photoFrame.navigation.onRightButtonChanged(handler)
     }
 
     //% block
-    //% group="Basic"
+    //% group="Voice"
     export function onCommandHeard(commandType: VoiceRecognitionCommandType, handler: () => void) {
         photoFrame.voiceRecognition.onCommandHeard(commandType, handler)
     }
 
 
-    //% block
-    //% group="Basic"
+
+    
+    //% block="start Automatic Control at Position $value|position shuffle position $isRandom"
+    //% group="Automatic Control"
+    //% inlineInputMode=inline
     export function startAutomaticControlAtPosition(position: number, isRandom: boolean) {
         photoFrame.lights.changeLedColor(isRandom ? NeoPixelColors.Blue : NeoPixelColors.White);
         photoFrame.automaticControl.startAutomaticControl(position, isRandom)
     }
 
     //% block
-    //% group="Basic"
+    //% group="Automatic Control"
     export function onAutomaticPositionChanged(handler: () => void) {
         photoFrame.automaticControl.setAutomicControlEmissionsCallback(handler)
     }
 
     //% block
-    //% group="Basic"
+    //% group="Automatic Control"
     export function getAutomaticPosition() {
         return photoFrame.automaticControl.position;
     }
 
 
     //% block
-    //% group="Basic"
+    //% group="Automatic Control"
     export function stopAutomicControl() {
         photoFrame.lights.changeLedColor(NeoPixelColors.Green);
         photoFrame.automaticControl.stop();
