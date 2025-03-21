8e1089241629e4e1c3ea3453acf3ca7c49530c5f
robot-ie <mig.ruiz+1@gmail.com>
2025-03-21 11:52:46 +0000

    image

--- Display.ts
+++ Display.ts
@@ -6,7 +6,9 @@ namespace photoFrame {
 
         }
         showPhotoAtPosition(position:number){
-            lcdDisplay.lcdDisplayText(convertToText(position), 1, 120, 120, lcdDisplay.FontSize.Large, 0xFF0000)
+            //lcdDisplay.lcdDisplayText(convertToText(position), 1, 120, 120, lcdDisplay.FontSize.Large, 0xFF0000)
+            const path = `/trip/${position}.png`;
+            lcdDisplay.lcdDisplayIamge(1, path, 0, 0, 256)
         }
         init() {
             lcdDisplay.lcdInitIIC()            
