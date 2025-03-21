a86acb2ae950d4eb994bcbdf1d5acd4595a80bd4
robot-ie <mig.ruiz+1@gmail.com>
2025-03-21 10:35:13 +0000

    refactor

--- Navigation.ts
+++ Navigation.ts
@@ -2,16 +2,23 @@ namespace photoFrame {
     class TouchButton {
         private lastTimeOnTimestamp: number = 0
         private pin: DigitalPin
+        private handler: () => void
         constructor(pin: DigitalPin) {
             this.pin = pin;
         }
-        evaluate(handler: () => void) {
+        setHandler(handler:()=>void){
+            this.handler = handler;
+        }
+        evaluate() {
+            if (!this.handler){
+                return;
+            }
             const currentTime = control.millis()
             const buttonSignal = pins.digitalReadPin(DigitalPin.P0)
             if (buttonSignal == 1) {
                 if (currentTime >= this.lastTimeOnTimestamp + 1000) {
                     this.lastTimeOnTimestamp = currentTime
-                    handler();
+                    this.handler();
                 }
             }
         }
@@ -20,25 +27,16 @@ namespace photoFrame {
         private leftButton: TouchButton = new TouchButton(DigitalPin.P1)
         private rightButton: TouchButton = new TouchButton(DigitalPin.P0)
 
-        private callBacks: {
-            left: () => void,
-            right: () => void
-        }
         constructor() {
 
         }
         onLeftButtonChanged(handler: () => void) {
-            this.callBacks.left = handler;
+            this.leftButton.setHandler(handler)
 
         }
         init() {
             basic.forever(function () {
-                if (this.callBacks.left){
-                    this.leftButton.evaluate(this.callBacks.left);
-                }
-                if (this.callBacks.right) {
-                    this.rightButton.evaluate(this.callBacks.right);
-                }
+                this.leftButton.evaluate();
             })
         }
 
