17c4b81ccde107e62d2b574ac8b267fe88870226
robot-ie <mig.ruiz+1@gmail.com>
2025-03-21 10:41:19 +0000

    extracting class

--- Navigation.ts
+++ Navigation.ts
@@ -1,31 +1,5 @@
 namespace photoFrame {
-    class TouchButton {
-        private lastTimeOnTimestamp: number = 0
-        private pin: DigitalPin
-        private handler: () => void
-        constructor(pin: DigitalPin) {
-            this.pin = pin;
-        }
-        setHandler(handler: () => void) {
-            this.handler = handler;
-        }
-        evaluate() {
-            if (!this.handler) {
-                return;
-            }
-            const currentTime = control.millis()
-            const buttonSignal = pins.digitalReadPin(this.pin)
-            if (buttonSignal == 1) {
-                if (currentTime >= this.lastTimeOnTimestamp + 1000) {
-                    this.lastTimeOnTimestamp = currentTime
-                    this.handler();
-                }
-            }
-            else{
-                this.lastTimeOnTimestamp = 0
-            }
-        }
-    }
+
     export class Navigation {
         private leftButton: TouchButton = new TouchButton(DigitalPin.P1)
         private rightButton: TouchButton = new TouchButton(DigitalPin.P0)
