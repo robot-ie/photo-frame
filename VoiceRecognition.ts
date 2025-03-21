94e98bcfb454a9182e133d99169606c84731f9cb
robot-ie <mig.ruiz+1@gmail.com>
2025-03-21 10:53:20 +0000

    Update VoiceRecognition.ts

--- VoiceRecognition.ts
+++ VoiceRecognition.ts
@@ -1,1 +1,24 @@
+namespace photoFrame {
 
+    export class VoiceRecognition {
+        private leftButton: TouchButton = new TouchButton(DigitalPin.P1)
+        private rightButton: TouchButton = new TouchButton(DigitalPin.P0)
+
+        constructor() {
+
+        }
+        onLeftButtonChanged(handler: () => void) {
+            this.leftButton.setHandler(handler)
+        }
+        onRightButtonChanged(handler: () => void) {
+            this.rightButton.setHandler(handler)
+        }
+        init() {
+            basic.forever(function () {
+                this.leftButton.evaluate();
+                this.rightButton.evaluate();
+            })
+        }
+
+    }
+}
